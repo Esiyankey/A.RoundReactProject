1eeb9a4bfcbd82b5cff3b422ee29c638716644a4
Esi Yankey <graceyan244@gmail.com>
2023-11-15 10:23:18 +0000

    added the sign up page

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,6 +1,9 @@
 import React from "react";
+import { BsTwitter } from "react-icons/bs";
+import { FcGoogle } from "react-icons/fc";
 import musicImage from "../assets/music-image2.jpg";
 import "../styles/signup.css";
+import { Link } from "react-router-dom";
 export const SignUp = () => {
   return (
     <div className="bg-white flex  h-[100vh] justify-center items-center text-center ">
@@ -15,19 +18,28 @@ export const SignUp = () => {
             </div>
           </div> */}
       {/* <div className=" p-12 flex justify-center items-center bg-white "> */}
-      <div className=" p-10 w-[450px] rounded-lg bg-white shadow-custom ">
+      <div className=" p-10 w-[500px] rounded-lg bg-white shadow-custom ">
         <h1 className="text-5xl font-bold z-20 ">Getting Started</h1>
         <h3 className="text-sm my-2">
           Sign up now – it's quick, easy, and hassle-free!
         </h3>
         <form>
-          <div>
-            <button className="flex items-center bg-blue-500">
-              <button></button>
-              signup with google
-            </button>
+          <div className="flex items-center mt-9 mb-5 gap-2">
+            <div>
+              <button className="flex gap-1  items-center backgroundGradientButton button">
+                <button className="p-2 bg-white rounded">
+                  <FcGoogle />
+                </button>
+                Sign up with google
+              </button>
+            </div>
+            <div>
+              <button className="text-center rounded p-[0.65rem] text-blue-600 text-xl bg-[#f3f3f4] flex justify-center items-center">
+                <BsTwitter />
+              </button>
+            </div>
           </div>
-          <div className="flex gap-2 my-8">
+          <div className="flex gap-2 ">
             <div className="flex flex-col items-start ">
               <label>Name</label>
               <input placeholder="name" type="text" />
@@ -49,7 +61,10 @@ export const SignUp = () => {
             <label>Confirm Password</label>
             <input placeholder="confirm password" type="password" />
           </div>
-          <button className="">Create Account</button>
+          <button className=" w-full backgroundGradientButton mt-6 px-3 py-2 rounded text-white text-xl ">
+            Create Account
+          </button>
+          <p>Already have an account? <Link to="/login" className="text-[#e72eb6]">Login</Link></p>
         </form>
       </div>
       {/* </div> */}
