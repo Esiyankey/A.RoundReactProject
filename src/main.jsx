546dafb4611b8594838ad2a248b13c2ac1e6ed01
Esi Yankey <graceyan244@gmail.com>
2023-12-05 15:29:33 +0000

    changes made

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Navbar from "./components/navbar.jsx";
+import Navbar from "./components/Navbar.jsx";
 import ErrorPage from "./components/ErrorPage.jsx";
 import Footer from "./components/Footer.jsx";
 import Contact from "./components/Contact.jsx"
